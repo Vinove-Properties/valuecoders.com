18082f0ecf03badf35b28c4b39d164cf9f21e7db
nandani <nandani.sinha@mail.vinove.com>
2024-05-06 11:45:52 +0530

    Page Template Update

--- staging/wp-content/themes/valuecoders/page-templates/template-services-v2.php
+++ staging/wp-content/themes/valuecoders/page-templates/template-services-v2.php
@@ -72,7 +72,7 @@
         </div>
         <div class="btn-sec margin-t-50 ">
           <a href="https://www.valuecoders.com/v2wp/contact" class="btn rounded">
-          <span class="text-white">Get Free Consulation</span>
+          <span class="text-white">Get Free Consultation</span>
           </a>
         </div>
       </div>
