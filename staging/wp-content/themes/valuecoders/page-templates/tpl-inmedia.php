c09328297a3dcefa594e0baaace1f4e6aa59ea1c
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-05-27 14:43:54 +0530

    in media template update

--- staging/wp-content/themes/valuecoders/page-templates/tpl-inmedia.php
+++ staging/wp-content/themes/valuecoders/page-templates/tpl-inmedia.php
@@ -233,7 +233,11 @@
       		<div class="media-box">';
       		$tlightlg 		= $row['logo-light'];
       		$tlightlgwp 	= $row['logo-light-wp'];
-      		if( $tlightlg && $tlightlgwp ){
+          if( $tlightlg ){
+            echo vc_pictureElm( $tlightlg );
+          }
+      		/*
+          if( $tlightlg && $tlightlgwp ){
       		echo '<picture>
       		<source type="image/webp" srcset="'.$tlightlgwp['url'].'">
       		<source type="'.$tlightlg['mime_type'].'" srcset="'.$tlightlg['url'].'">
@@ -241,6 +245,7 @@
       		height="'.$tlightlg['height'].'"> 
       		</picture>';
       		}
+          */
       		echo '<div class="content-box">
       		<p>'.$row['content'].'</p>
       		</div>
