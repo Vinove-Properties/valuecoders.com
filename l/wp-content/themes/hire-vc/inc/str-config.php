56cd2fbea5dc53b86c138a063d403da1c4923ea8
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-07-16 10:54:35 +0530

    removed lp live instance from git

--- l/wp-content/themes/hire-vc/inc/str-config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$itemName = "Demo Product";
-$itemPrice = 25;
-$currency = "INR";
-?>
--- l/wp-content/themes/hire-vc/inc/str-dbConnect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$db = new mysqli('localhost', 'phpmyadmin', 'root', 'valuecoc_l7');  
-if ($db->connect_errno) {  
-    printf("Connect failed: %s\n", $db->connect_error);  
-    exit();  
-}
-?>
