8b032172462153466821de5c367da9af2b234723
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-08-08 11:15:21 +0530

    lp script update

--- sendmail-l7.php
+++ sendmail-l7.php
@@ -1,8 +1,8 @@
 <?php
 //print_r($_POST); die;
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
