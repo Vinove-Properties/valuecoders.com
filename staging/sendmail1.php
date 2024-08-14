d971b9e6090200a27903c3c2bf366e365622120c
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-08-14 13:14:30 +0530

    sendmail script update

--- staging/sendmail1.php
+++ staging/sendmail1.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 if( ($_SERVER['REQUEST_METHOD'] == 'GET') && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ){
     header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
     die("HEY BOAT.. Go Away");
