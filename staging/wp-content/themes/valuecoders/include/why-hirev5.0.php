39c053fd506abc4512487a4734d5a92e0ba16427
nandani <nandani.sinha@mail.vinove.com>
2024-12-06 11:21:09 +0530

    WHY Hire changes

--- staging/wp-content/themes/valuecoders/include/why-hirev5.0.php
+++ staging/wp-content/themes/valuecoders/include/why-hirev5.0.php
@@ -25,192 +25,144 @@
 
       <div class="flex-2 right-box">
           <div class="client-section">
-            <div class="client-row">
-              <div class="client-stack award-animate-slide-to-left hover:pause">
-                <ul>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.png" alt="Valuecoders" width="76" height="29">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo2.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo2.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo2.png" alt="Valuecoders" width="100" height="15">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo3.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo3.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo3.png" alt="Valuecoders" width="64" height="18">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo4.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo4.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo4.png" alt="Valuecoders" width="45" height="31">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.png" alt="Valuecoders" width="76" height="29">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo2.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo2.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo2.png" alt="Valuecoders" width="100" height="15">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo3.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo3.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo3.png" alt="Valuecoders" width="64" height="18">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo4.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo4.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo4.png" alt="Valuecoders" width="45" height="31">
-                    </picture>
-                  </li>
-                </ul>
-              </div>
+          <div class="client-row">
+            <div class="client-stack award-animate-slide-to-left hover:pause">
+              <ul>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-01.png" alt="Valuecoders" width="76" height="29">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-02.png" alt="Valuecoders" width="113" height="17">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-03.png" alt="Valuecoders" width="85" height="28">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-04.png" alt="Valuecoders" width="68" height="46">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-05.png" alt="Valuecoders" width="91" height="40">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-06.png" alt="Valuecoders" width="98" height="49">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-07.png" alt="Valuecoders" width="80" height="33">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-08.png" alt="Valuecoders" width="110" height="50">
+                  </picture>
+                </li>
+              </ul>
             </div>
-            <div class="client-row">
-              <div class="client-stack award-animate-slide-to-right hover:pause">
-                <ul>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo5.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo5.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo5.png" alt="Valuecoders" width="61" height="26">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo6.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo6.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo6.png" alt="Valuecoders" width="87" height="44">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo7.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo7.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo7.png" alt="Valuecoders" width="56" height="23">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo8.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo8.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo8.png" alt="Valuecoders" width="93" height="50">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo5.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo5.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo5.png" alt="Valuecoders" width="61" height="26">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo6.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo6.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo6.png" alt="Valuecoders" width="87" height="44">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo7.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo7.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo7.png" alt="Valuecoders" width="56" height="23">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo8.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo8.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo8.png" alt="Valuecoders" width="93" height="50">
-                    </picture>
-                  </li>
-                </ul>
-              </div>
+          </div>
+          <div class="client-row">
+            <div class="client-stack award-animate-slide-to-right hover:pause">
+              <ul>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-09.png" alt="Valuecoders" width="83" height="82">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-10.png" alt="Valuecoders" width="63" height="29">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-11.png" alt="Valuecoders" width="102" height="32">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-12.png" alt="Valuecoders" width="92" height="53">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-13.png" alt="Valuecoders" width="61" height="26">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-14.png" alt="Valuecoders" width="87" height="44">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-15.png" alt="Valuecoders" width="94" height="38">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-16.png" alt="Valuecoders" width="93" height="50">
+                  </picture>
+                </li>
+              </ul>
             </div>
-            <div class="client-row">
-              <div class="client-stack award-animate-slide-to-left hover:pause">
-                <ul>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo9.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo9.png" alt="Valuecoders" width="68" height="35">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo10.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo10.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo10.png" alt="Valuecoders" width="63" height="28">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo11.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo11.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo11.png" alt="Valuecoders" width="82" height="26">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo12.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo12.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo12.png" alt="Valuecoders" width="63" height="24">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo9.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo9.png" alt="Valuecoders" width="68" height="35">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo10.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo10.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo10.png" alt="Valuecoders" width="63" height="28">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo11.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo11.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo11.png" alt="Valuecoders" width="82" height="26">
-                    </picture>
-                  </li>
-                  <li>
-                    <picture>
-                      <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo12.webp">
-                      <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo12.png">
-                      <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v5.0/images/comp-logo12.png" alt="Valuecoders" width="63" height="24">
-                    </picture>
-                  </li>
-                </ul>
-              </div>
+          </div>
+          <div class="client-row">
+            <div class="client-stack award-animate-slide-to-left hover:pause">
+              <ul>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-17.png" alt="Valuecoders" width="104" height="30">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-18.png" alt="Valuecoders" width="96" height="30">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-19.png" alt="Valuecoders" width="50" height="50">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-20.png" alt="Valuecoders" width="82" height="49">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-21.png" alt="Valuecoders" width="109" height="27">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-22.png" alt="Valuecoders" width="103" height="54">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-23.png" alt="Valuecoders" width="94" height="50">
+                  </picture>
+                </li>
+                <li>
+                  <picture>
+                    <img loading="lazy" src="<?php bloginfo('template_url'); ?>/v4.0/images/whyI-24.png" alt="Valuecoders" width="100" height="33">
+                  </picture>
+                </li>
+              </ul>
             </div>
+          </div>
             <div class="award-sc">
               <h5>Awards & Certifications -</h5>
               <div class="award-row">
