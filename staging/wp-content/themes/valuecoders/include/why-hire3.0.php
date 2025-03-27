593fb6e638fa30c4bfb03bfdde9ac4aca9b1b743
Nitin Baluni <nitin.baluni@mail.vinove.com>
2025-03-27 11:40:37 +0530

    hire template update

--- staging/wp-content/themes/valuecoders/include/why-hire3.0.php
+++ staging/wp-content/themes/valuecoders/include/why-hire3.0.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="head-txt text-center">
       <h2>Why Hire Developers From ValueCoders?</h2>
-      <p>Accelerate your product development with elite developers from one of India’s most trusted software engineering partners. Our remote developers don’t just code - they align with your business goals, integrate seamlessly with your teams, and start delivering value from day one.</p>
+      <p>Accelerate your product development with experienced developers from one of India’s most trusted software engineering partners. Our remote developers don’t just code - they align with your business goals, integrate seamlessly with your teams, and start delivering value from day one.</p>
     </div>
     <div class="dis-flex  col-box-outer">
       <div class="row-box dis-flex justify-sb items-center">
@@ -27,8 +27,8 @@
         <div class="flex-2">
           <h3>Rigorous Vetting Beyond Just Coding</h3>
           <ul>
-            <li>5+ hours of assessments on DSA, system design, frameworks, and soft skills</li>
-            <li>Seniority tests to ensure proactive problem solvers - not just coders. Only client-ready engineers get through</li>
+            <li>5+ hours of assessments on DSA, system design, frameworks, and soft skills.</li>
+            <li>Seniority tests to ensure proactive problem solvers - not just coders. Only client-ready engineers get through.</li>
           </ul>
         </div>
         <div class="flex-2">
@@ -45,7 +45,7 @@
         <div class="flex-2">
           <h3>Full Transparency & Productivity Insights</h3>
           <ul>
-            <li>Powered by Workstatus™, our developers log proof-of-work timesheets, track task-level progress, and join daily standups.</li>
+            <li>Powered by <a href="https://www.workstatus.io/" target="_blank">Workstatus<sup>TM</sup></a>, our developers log proof-of-work timesheets, track task-level progress, and join daily standups.</li>
             <li>Our developers match your time zone and overlap a minimum of 4 hours with your workday.</li>
           </ul>
         </div>
