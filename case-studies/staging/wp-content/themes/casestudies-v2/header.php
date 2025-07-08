80db068953d482ea890e91685cb27c5acb5dbe8b
Nitin Baluni <nitin.baluni@mail.vinove.com>
2025-07-08 14:43:14 +0530

    vc case studies - header update

--- case-studies/staging/wp-content/themes/casestudies-v2/header.php
+++ case-studies/staging/wp-content/themes/casestudies-v2/header.php
@@ -21,18 +21,21 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <?php wp_head(); ?>
 <script>
-	function fun() {
+function fun() {
 	var element = document.getElementById("myDIV");
 	element.classList.toggle("open");
 
 	var element = document.getElementById("shareDiv");
 	element.classList.add("active");
- }</script>	
-<script type="text/javascript">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+}
+</script>	
+<script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://gtm.valuecoders.com';f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PL37X57');</script>
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PL37X57');
+</script>
 
 <link type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/glider.css" rel="preload stylesheet" as="style" />	
 </head>
